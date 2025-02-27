06acc8fd8ba05bc4c552d2f87775a85cc14a6667
abhay <abhay.singh@tothenew.comgit config --global user.name abhaygit config --global user.email abhay.singh@tothenew.com>
2025-02-27 20:45:34 +0530

    fix:url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const BASE_URL = process.env.HOSTED_URL || "http://localhost:3000";
-
 export default NextAuth({
   providers: [
     GoogleProvider({
